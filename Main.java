057e86a24df437a61c5653be20bd2f43de420764
Agent-05 <falkwish15@gmail.com>
2024-05-06 13:32:04 -0500

    map for the most part made

--- Main.java
+++ Main.java
@@ -3,5 +3,7 @@ public class Main {
 
     public static void main(String[] args) {
         WumpusFrame wf = new WumpusFrame();
+        WumpusMap w = new WumpusMap();
+        System.out.println(w.toString());
     }
 }
--- WumpusMap.java
+++ WumpusMap.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.*;
+
 public class WumpusMap {
     WumpusMap(){createMap();}
 
@@ -5,18 +8,119 @@ public class WumpusMap {
     public static final int numColumns = 10;
     public static final int numPits = 10;
 
-    private WumpusSquare[][] grid;
+    private WumpusSquare[][] grid = new WumpusSquare[numRows][numColumns];
     static int ladderC;
     static int ladderR;
 
     void createMap(){
+        for (int i = 0; i < numRows;i++)
+        {
+            for (int a = 0; a < numColumns;a++)
+            {
+                WumpusSquare w = new WumpusSquare();
+                grid[i][a] = w;
+            }
+        }
+        Random rand = new Random();
+        int randX;
+        int randY;
+        for(int i = 0; i < 3; i++)
+        {
+            do{
+                 randX =rand.nextInt(10);
+                 randY =rand.nextInt(10);
+            }while(!grid[randY][randX].isEmpty());
+            grid[randY][randX].setEmpty(false);
+            if(i == 0)
+                grid[randY][randX].setGold(true);
+            if(i == 1)
+            {
+                grid[randY][randX].setWumpus(true);
+                if(randY != 0)
+                {
+                    grid[randY-1][randX].setEmpty(false);
+                    grid[randY-1][randX].setStench(true);
+                }
+                if(randY != 9)
+                {
+                    grid[randY+1][randX].setEmpty(false);
+                    grid[randY+1][randX].setStench(true);
+                }
+                if(randX != 0)
+                {
+                    grid[randY][randX-1].setEmpty(false);
+                    grid[randY][randX-1].setStench(true);
+                }
+                if(randX != 9)
+                {
+                    grid[randY][randX+1].setEmpty(false);
+                    grid[randY][randX+1].setStench(true);
+                }
+            }
+            if(i == 2)
+                grid[randY][randX].setLadder(true);
+        }
+        //goes to place pits
+        for(int i = 0; i < numPits; i++)
+        {
+            randX = rand.nextInt(10);
+            randY = rand.nextInt(10);
+            if(grid[randY][randX].isEmpty())
+            {
+                //puts breeze around holes
+                grid[randY][randX].setEmpty(false);
+                grid[randY][randX].setPit(true);
+                if(randY != 0)
+                {
+                    grid[randY-1][randX].setEmpty(false);
+                    grid[randY-1][randX].setBreeze(true);
+                }
+                if(randY != 9)
+                {
+                    grid[randY+1][randX].setEmpty(false);
+                    grid[randY+1][randX].setBreeze(true);
+                }
+                if(randX != 0)
+                {
+                    grid[randY][randX-1].setEmpty(false);
+                    grid[randY][randX-1].setBreeze(true);
+                }
+                if(randX != 9)
+                {
+                    grid[randY][randX+1].setEmpty(false);
+                    grid[randY][randX+1].setBreeze(true);
+                }
+
+            }else{
+                i-=1;
+            }
+        }
+
+    }
+
+    public ArrayList<WumpusSquare> map(){
+        ArrayList<WumpusSquare> b = new ArrayList<>();
+        for(int i = 0; i< 10;i++)
+        {
+            for (int a = 0; a<10;a++)
+            {
+                b.add(grid[i][a]);
+            }
+        }
     }
 
     int getLadderCol() {return numColumns;}
     int getLadderRows() {return numRows;}
-    WumpusSquare
     @Override
-    public String toString(){
+    public  String toString(){
+        for(int i = 0; i <10; i++)
+        {
+            for(int a = 0; a < 10; a++)
+            {
+                System.out.print(grid[i][a].toString());
+            }
+            System.out.println("\n");
+        }
         return "";
     }
 
